ca199f8890791b02ea6e9e7adea1bdb9950ed56a
Marcos Eduardo <44882253+MR-MARCOSs@users.noreply.github.com>
2025-04-29 18:25:58 +0000

    english page updated

---
+++ english/EnCertificates.js
@@ -0,0 +1,85 @@
+// certificates.js
+const certificates = [
+  {
+    title: "Intro to Machine Learning",
+    issuer: "Kaggle",
+    image: "../assets/intro-to-ml.png",
+    link: "../assets/intro-to-ml.png",
+    alt: "Project 1"
+  },
+  {
+    title: "Intermediate Machine Learning",
+    issuer: "Kaggle",
+    image: "../assets/intermediate-ml.png",
+    link: "../assets/intermediate-ml.png",
+    alt: "Project 2"
+  },
+  {
+    title: "NodeJS",
+    issuer: "RocketSeat",
+    image: "../assets/nodejsimg.png",
+    link: "../assets/Nodejs.pdf",
+    alt: "Project 3"
+  },
+  {
+    title: "Discover (HTML, CSS)",
+    issuer: "RocketSeat",
+    image: "../assets/discover.png",
+    link: "../assets/Discover.pdf",
+    alt: "Project 4"
+  },
+  {
+    title: "AI for Devs",
+    issuer: "RocketSeat",
+    image: "../assets/AI.png",
+    link: "../assets/AIForDevs.pdf",
+    alt: "Project 5"
+  },
+  {
+    title: "DevOps and Cloud Challenge",
+    issuer: "Fabricio Veronez",
+    image: "../assets/DevOps Badges.png",
+    link: "https://api.badgr.io/public/collections/75d41e9266b44192a0d25aa55b3a47fc",
+    alt: "Project 6"
+  }
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+  const projectCardsContainer = document.getElementById('project-cards');
+
+  // Iterates over the certificates list and creates HTML elements dynamically
+  certificates.forEach(cert => {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project-card');
+
+    const projectContent = document.createElement('div');
+    projectContent.classList.add('project-content');
+
+    const projectTitle = document.createElement('h3');
+    projectTitle.textContent = cert.title;
+
+    const projectIssuer = document.createElement('h4');
+    projectIssuer.textContent = `By: ${cert.issuer}`;
+
+    const projectImage = document.createElement('img');
+    projectImage.src = cert.image;
+    projectImage.alt = cert.alt;
+    projectImage.classList.add('project-image');
+
+    const certificateLink = document.createElement('a');
+    certificateLink.href = cert.link;
+    certificateLink.target = "_blank";
+    certificateLink.classList.add('certificate-link');
+    certificateLink.textContent = "View certificate";
+
+    // Adds elements to the project content
+    projectContent.appendChild(projectTitle);
+    projectContent.appendChild(projectIssuer);
+    projectContent.appendChild(projectImage);
+    projectContent.appendChild(certificateLink);
+
+    // Adds the card to the certificates list
+    projectCard.appendChild(projectContent);
+    projectCardsContainer.appendChild(projectCard);
+  });
+});
---
+++ english/EnProjects.js
@@ -0,0 +1,85 @@
+// projects.js
+const projects = [
+  {
+    title: "AI Chat Website",
+    image: "../assets/project1.png",
+    alt: "Project 1",
+    description: "Website with database and JWT authentication to chat with AIs, hosted on Render.",
+    tags: ["TypeScript", "NodeJS", "MySQL"],
+    githubLink: "https://github.com/MR-MARCOSs/site_bots",
+    githubIcon: "../assets/github.png"
+  },
+  {
+    title: "Langchain Agent API",
+    image: "../assets/project2.png",
+    alt: "Project 2",
+    description: "Agent with tools created using Langchain consuming OpenAI's API.",
+    tags: ["Python", "Flask", "Langchain"],
+    githubLink: "https://huggingface.co/spaces/MR-MARCOSs/Goobe/tree/main",
+    githubIcon: "../assets/hf-logo.svg"
+  },
+  {
+    title: "Cryptocurrency Favorites",
+    image: "../assets/project3.png",
+    alt: "Project 3",
+    description: "Application to view prices and favorite cryptocurrencies.",
+    tags: ["Flutter", "Dart", "Provider"],
+    githubLink: "https://github.com/MR-MARCOSs/cryptoWeb",
+    githubIcon: "../assets/github.png"
+  },
+  
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+  const projectsContainer = document.getElementById('projects-container');
+
+  projects.forEach(project => {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project-card');
+
+    const projectContent = document.createElement('div');
+    projectContent.classList.add('project-content');
+
+    const projectTitle = document.createElement('h3');
+    projectTitle.textContent = project.title;
+
+    const projectImage = document.createElement('img');
+    projectImage.src = project.image;
+    projectImage.alt = project.alt;
+    projectImage.classList.add('project-image');
+
+    const projectDescription = document.createElement('p');
+    projectDescription.classList.add('project-description');
+    projectDescription.textContent = project.description;
+
+    const tagsContainer = document.createElement('div');
+    tagsContainer.classList.add('tags');
+    project.tags.forEach(tag => {
+      const tagElement = document.createElement('span');
+      tagElement.classList.add('tag');
+      tagElement.textContent = tag;
+      tagsContainer.appendChild(tagElement);
+    });
+
+    projectContent.appendChild(projectTitle);
+    projectContent.appendChild(projectImage);
+    projectContent.appendChild(projectDescription);
+    projectContent.appendChild(tagsContainer);
+
+    const projectLink = document.createElement('a');
+    projectLink.href = project.githubLink;
+    projectLink.target = "_blank";
+    projectLink.classList.add('github-icon');
+
+    const githubIcon = document.createElement('img');
+    githubIcon.src = project.githubIcon;
+    githubIcon.alt = "GitHub";
+
+    projectLink.appendChild(githubIcon);
+
+    projectCard.appendChild(projectContent);
+    projectCard.appendChild(projectLink);
+
+    projectsContainer.appendChild(projectCard);
+  });
+});
